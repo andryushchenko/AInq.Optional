945ca7e4bf6b2fac7d21519479fad08c9e02cffd
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2022-03-15 20:29:21 +0300

    [REF] Using directives cleanup [UPD] CI

--- src/AInq.Optional.Async/TryAsync.cs
+++ src/AInq.Optional.Async/TryAsync.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using JetBrains.Annotations;
 using System.Runtime.CompilerServices;
 using static AInq.Optional.TaskHelper;
 
