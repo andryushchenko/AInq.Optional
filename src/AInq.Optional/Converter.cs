39cadc3fe7beb30d22a6fc44274a4b80af72ef57
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2025-11-12 15:44:49 +0300

    [REF] Extension block

--- src/AInq.Optional/Converter.cs
+++ src/AInq.Optional/Converter.cs
@@ -17,119 +17,105 @@ namespace AInq.Optional;
 /// <summary> Converter utils </summary>
 public static class Converter
 {
-#region Maybe
-
-    /// <summary> Get left value or none </summary>
     /// <param name="either"> Either source </param>
     /// <typeparam name="TLeft"> Left source type </typeparam>
     /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Maybe with left value </returns>
-    [PublicAPI, Pure]
-    public static Maybe<TLeft> MaybeLeft<TLeft, TRight>(this Either<TLeft, TRight> either)
-        => (either ?? throw new ArgumentNullException(nameof(either))).HasLeft ? Maybe.Value(either.Left) : Maybe.None<TLeft>();
+    extension<TLeft, TRight>(Either<TLeft, TRight> either)
+    {
+        /// <summary> Get left value or none </summary>
+        /// <returns> Maybe with left value </returns>
+        [PublicAPI, Pure]
+        public Maybe<TLeft> MaybeLeft()
+            => (either ?? throw new ArgumentNullException(nameof(either))).HasLeft ? Maybe.Value(either.Left) : Maybe.None<TLeft>();
 
-    /// <summary> Get right value or none </summary>
+        /// <summary> Get right value or none </summary>
+        /// <returns> Maybe with right value </returns>
+        [PublicAPI, Pure]
+        public Maybe<TRight> MaybeRight()
+            => (either ?? throw new ArgumentNullException(nameof(either))).HasRight ? Maybe.Value(either.Right) : Maybe.None<TRight>();
+    }
+
     /// <param name="either"> Either source </param>
     /// <typeparam name="TLeft"> Left source type </typeparam>
     /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Maybe with right value </returns>
-    [PublicAPI, Pure]
-    public static Maybe<TRight> MaybeRight<TLeft, TRight>(this Either<TLeft, TRight> either)
-        => (either ?? throw new ArgumentNullException(nameof(either))).HasRight ? Maybe.Value(either.Right) : Maybe.None<TRight>();
+    extension<TLeft, TRight>(Either<TLeft, TRight> either)
+    {
+        /// <summary> Try get left value </summary>
+        /// <returns> Try with left value </returns>
+        [PublicAPI, Pure]
+        public Try<TLeft> TryLeft()
+            => (either ?? throw new ArgumentNullException(nameof(either))).HasLeft
+                ? Try.Value(either.Left)
+                : Try.Error<TLeft>(new InvalidOperationException("No left value"));
 
-    /// <summary> Convert <see cref="Try{T}" /> to <see cref="Maybe{T}" /> </summary>
-    /// <param name="try"> Try item </param>
-    /// <typeparam name="T"> Value type </typeparam>
-    [PublicAPI, Pure]
-    public static Maybe<T> AsMaybe<T>(this Try<T> @try)
-        => (@try ?? throw new ArgumentNullException(nameof(@try))).Success ? Maybe.Value(@try.Value) : Maybe.None<T>();
+        /// <summary> Try get right value </summary>
+        /// <returns> Try with right value </returns>
+        [PublicAPI, Pure]
+        public Try<TRight> TryRight()
+            => (either ?? throw new ArgumentNullException(nameof(either))).HasRight
+                ? Try.Value(either.Right)
+                : Try.Error<TRight>(new InvalidOperationException("No right value"));
+    }
 
-#endregion
-
-#region Try
-
-    /// <summary> Try get left value </summary>
-    /// <param name="either"> Either source </param>
-    /// <typeparam name="TLeft"> Left source type </typeparam>
-    /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Try with left value </returns>
-    [PublicAPI, Pure]
-    public static Try<TLeft> TryLeft<TLeft, TRight>(this Either<TLeft, TRight> either)
-        => (either ?? throw new ArgumentNullException(nameof(either))).HasLeft
-            ? Try.Value(either.Left)
-            : Try.Error<TLeft>(new InvalidOperationException("No left value"));
-
-    /// <summary> Try get right value </summary>
-    /// <param name="either"> Either source </param>
-    /// <typeparam name="TLeft"> Left source type </typeparam>
-    /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Try with right value </returns>
-    [PublicAPI, Pure]
-    public static Try<TRight> TryRight<TLeft, TRight>(this Either<TLeft, TRight> either)
-        => (either ?? throw new ArgumentNullException(nameof(either))).HasRight
-            ? Try.Value(either.Right)
-            : Try.Error<TRight>(new InvalidOperationException("No right value"));
-
-    /// <summary> Convert <see cref="Maybe{T}" /> to <see cref="Try{T}" /> </summary>
-    /// <param name="maybe"> Maybe item </param>
-    /// <typeparam name="T"> Value type </typeparam>
-    [PublicAPI, Pure]
-    public static Try<T> AsTry<T>(this Maybe<T> maybe)
-        => (maybe ?? throw new ArgumentNullException(nameof(maybe))).HasValue
-            ? Try.Value(maybe.Value)
-            : Try.Error<T>(new InvalidOperationException("No value"));
-
-#endregion
-
-#region Or
-
-    /// <summary> Get source value or other if empty </summary>
     /// <param name="maybe"> Maybe source </param>
-    /// <param name="other"> Other value </param>
     /// <typeparam name="TLeft"> Left source type </typeparam>
-    /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Either </returns>
-    [PublicAPI, Pure]
-    public static Either<TLeft, TRight> Or<TLeft, TRight>(this Maybe<TLeft> maybe, [NoEnumeration] TRight other)
-        => (maybe ?? throw new ArgumentNullException(nameof(maybe))).HasValue
-            ? Either.Left<TLeft, TRight>(maybe.Value)
-            : Either.Right<TLeft, TRight>(other);
+    extension<TLeft>(Maybe<TLeft> maybe)
+    {
+        /// <summary> Get source value or other if empty </summary>
+        /// <param name="other"> Other value </param>
+        /// <typeparam name="TRight"> Right source type </typeparam>
+        /// <returns> Either </returns>
+        [PublicAPI, Pure]
+        public Either<TLeft, TRight> Or<TRight>([NoEnumeration] TRight other)
+            => (maybe ?? throw new ArgumentNullException(nameof(maybe))).HasValue
+                ? Either.Left<TLeft, TRight>(maybe.Value)
+                : Either.Right<TLeft, TRight>(other);
 
-    /// <summary> Get source value or other if empty </summary>
-    /// <param name="maybe"> Maybe source </param>
-    /// <param name="otherGenerator"> Other generator </param>
-    /// <typeparam name="TLeft"> Left source type </typeparam>
-    /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Either </returns>
-    [PublicAPI, Pure]
-    public static Either<TLeft, TRight> Or<TLeft, TRight>(this Maybe<TLeft> maybe, [InstantHandle] Func<TRight> otherGenerator)
-        => (maybe ?? throw new ArgumentNullException(nameof(maybe))).HasValue
-            ? Either.Left<TLeft, TRight>(maybe.Value)
-            : Either.Right<TLeft, TRight>((otherGenerator ?? throw new ArgumentNullException(nameof(otherGenerator))).Invoke());
+        /// <summary> Get source value or other if empty </summary>
+        /// <param name="otherGenerator"> Other generator </param>
+        /// <typeparam name="TRight"> Right source type </typeparam>
+        /// <returns> Either </returns>
+        [PublicAPI, Pure]
+        public Either<TLeft, TRight> Or<TRight>([InstantHandle] Func<TRight> otherGenerator)
+            => (maybe ?? throw new ArgumentNullException(nameof(maybe))).HasValue
+                ? Either.Left<TLeft, TRight>(maybe.Value)
+                : Either.Right<TLeft, TRight>((otherGenerator ?? throw new ArgumentNullException(nameof(otherGenerator))).Invoke());
 
-    /// <summary> Get source value or other if exception </summary>
+        /// <summary> Convert <see cref="Maybe{T}" /> to <see cref="Try{T}" /> </summary>
+        [PublicAPI, Pure]
+        public Try<TLeft> AsTry()
+            => (maybe ?? throw new ArgumentNullException(nameof(maybe))).HasValue
+                ? Try.Value(maybe.Value)
+                : Try.Error<TLeft>(new InvalidOperationException("No value"));
+    }
+
     /// <param name="try"> Try source </param>
-    /// <param name="other"> Other value </param>
     /// <typeparam name="TLeft"> Left source type </typeparam>
-    /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Either </returns>
-    [PublicAPI, Pure]
-    public static Either<TLeft, TRight> Or<TLeft, TRight>(this Try<TLeft> @try, [NoEnumeration] TRight other)
-        => (@try ?? throw new ArgumentNullException(nameof(@try))).Success
-            ? Either.Left<TLeft, TRight>(@try.Value)
-            : Either.Right<TLeft, TRight>(other);
+    extension<TLeft>(Try<TLeft> @try)
+    {
+        /// <summary> Get source value or other if exception </summary>
+        /// <param name="other"> Other value </param>
+        /// <typeparam name="TRight"> Right source type </typeparam>
+        /// <returns> Either </returns>
+        [PublicAPI, Pure]
+        public Either<TLeft, TRight> Or<TRight>([NoEnumeration] TRight other)
+            => (@try ?? throw new ArgumentNullException(nameof(@try))).Success
+                ? Either.Left<TLeft, TRight>(@try.Value)
+                : Either.Right<TLeft, TRight>(other);
 
-    /// <summary> Get source value or other if exception </summary>
-    /// <param name="try"> Try source </param>
-    /// <param name="otherGenerator"> Other generator </param>
-    /// <typeparam name="TLeft"> Left source type </typeparam>
-    /// <typeparam name="TRight"> Right source type </typeparam>
-    /// <returns> Either </returns>
-    [PublicAPI, Pure]
-    public static Either<TLeft, TRight> Or<TLeft, TRight>(this Try<TLeft> @try, [InstantHandle] Func<TRight> otherGenerator)
-        => (@try ?? throw new ArgumentNullException(nameof(@try))).Success
-            ? Either.Left<TLeft, TRight>(@try.Value)
-            : Either.Right<TLeft, TRight>((otherGenerator ?? throw new ArgumentNullException(nameof(otherGenerator))).Invoke());
+        /// <summary> Get source value or other if exception </summary>
+        /// <param name="otherGenerator"> Other generator </param>
+        /// <typeparam name="TRight"> Right source type </typeparam>
+        /// <returns> Either </returns>
+        [PublicAPI, Pure]
+        public Either<TLeft, TRight> Or<TRight>([InstantHandle] Func<TRight> otherGenerator)
+            => (@try ?? throw new ArgumentNullException(nameof(@try))).Success
+                ? Either.Left<TLeft, TRight>(@try.Value)
+                : Either.Right<TLeft, TRight>((otherGenerator ?? throw new ArgumentNullException(nameof(otherGenerator))).Invoke());
 
-#endregion
+        /// <summary> Convert <see cref="Try{T}" /> to <see cref="Maybe{T}" /> </summary>
+        [PublicAPI, Pure]
+        public Maybe<TLeft> AsMaybe()
+            => (@try ?? throw new ArgumentNullException(nameof(@try))).Success ? Maybe.Value(@try.Value) : Maybe.None<TLeft>();
+    }
 }
---
+++ src/AInq.Optional/TryExtension.cs
@@ -0,0 +1,134 @@
+﻿// Copyright 2021 Anton Andryushchenko
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace AInq.Optional;
+
+/// <summary> <see cref="Try{T}" /> extensions </summary>
+public static class TryExtension
+{
+    /// <param name="try"> Try item </param>
+    /// <typeparam name="T"> Source value type </typeparam>
+    extension<T>(Try<T> @try)
+    {
+        /// <summary> Convert to other value type </summary>
+        /// <param name="selector"> Converter </param>
+        /// <typeparam name="TResult"> Result value type </typeparam>
+        [PublicAPI, Pure]
+        public Try<TResult> Select<TResult>([InstantHandle] Func<T, TResult> selector)
+            => !(@try ?? throw new ArgumentNullException(nameof(@try))).Success
+                ? Try<TResult>.ConvertError(@try)
+                : Try.Result(selector ?? throw new ArgumentNullException(nameof(selector)), @try.Value);
+
+        /// <inheritdoc cref="TryExtension.Select{T,TResult}(AInq.Optional.Try{T},System.Func{T,TResult})" />
+        [PublicAPI, Pure]
+        public Try<TResult> Select<TResult>([InstantHandle] Func<T, Try<TResult>> selector)
+            => !(@try ?? throw new ArgumentNullException(nameof(@try))).Success
+                ? Try<TResult>.ConvertError(@try)
+                : Try.Result(selector ?? throw new ArgumentNullException(nameof(selector)), @try.Value).Unwrap();
+
+        /// <summary> Get value form this item or other </summary>
+        /// <param name="other"> Other </param>
+        [PublicAPI, Pure]
+        public Try<T> Or(Try<T> other)
+            => (@try ?? throw new ArgumentNullException(nameof(@try))).Success ? @try : other ?? throw new ArgumentNullException(nameof(other));
+
+        /// <summary> Get value form this item or other </summary>
+        /// <param name="otherGenerator"> Other generator </param>
+        [PublicAPI, Pure]
+        public Try<T> Or([InstantHandle] Func<Try<T>> otherGenerator)
+            => (@try ?? throw new ArgumentNullException(nameof(@try))).Success
+                ? @try
+                : (otherGenerator ?? throw new ArgumentNullException(nameof(otherGenerator))).Invoke();
+
+        /// <summary> Try to execute action </summary>
+        /// <param name="valueAction"> Action if value exists </param>
+        /// <param name="errorAction"> Action if error </param>
+        [PublicAPI]
+        public void Do([InstantHandle] Action<T> valueAction, [InstantHandle] Action<Exception> errorAction)
+        {
+            if ((@try ?? throw new ArgumentNullException(nameof(@try))).Success)
+                (valueAction ?? throw new ArgumentNullException(nameof(valueAction))).Invoke(@try.Value);
+            else
+                try
+                {
+                    @try.Throw();
+                }
+                catch (Exception exception)
+                {
+                    (errorAction ?? throw new ArgumentNullException(nameof(errorAction))).Invoke(exception);
+                }
+        }
+
+        /// <summary> Try to execute action with additional argument </summary>
+        /// <param name="valueAction"> Action if value exists </param>
+        /// <param name="errorAction"> Action if error </param>
+        /// <param name="argument"> Additional action argument </param>
+        /// <typeparam name="TArgument"> Additional action argument type </typeparam>
+        [PublicAPI]
+        public void Do<TArgument>([InstantHandle] Action<T, TArgument> valueAction, [InstantHandle] Action<Exception> errorAction, TArgument argument)
+        {
+            if ((@try ?? throw new ArgumentNullException(nameof(@try))).Success)
+                (valueAction ?? throw new ArgumentNullException(nameof(valueAction))).Invoke(@try.Value, argument);
+            else
+                try
+                {
+                    @try.Throw();
+                }
+                catch (Exception exception)
+                {
+                    (errorAction ?? throw new ArgumentNullException(nameof(errorAction))).Invoke(exception);
+                }
+        }
+
+        /// <summary> Try to execute action with value </summary>
+        /// <param name="valueAction"> Action if value exists </param>
+        /// <param name="throwIfError"> Throw exception if item contains error </param>
+        [PublicAPI]
+        public void Do([InstantHandle] Action<T> valueAction, bool throwIfError = false)
+        {
+            if ((@try ?? throw new ArgumentNullException(nameof(@try))).Success)
+                (valueAction ?? throw new ArgumentNullException(nameof(valueAction))).Invoke(@try.Value);
+            else if (throwIfError) @try.Throw();
+        }
+
+        /// <summary> Try to execute action with value </summary>
+        /// <param name="valueAction"> Action if value exists </param>
+        /// <param name="argument"> Additional action argument </param>
+        /// <param name="throwIfError"> Throw exception if item contains error </param>
+        /// <typeparam name="TArgument"> Additional action argument type </typeparam>
+        [PublicAPI]
+        public void Do<TArgument>([InstantHandle] Action<T, TArgument> valueAction, TArgument argument, bool throwIfError = false)
+        {
+            if ((@try ?? throw new ArgumentNullException(nameof(@try))).Success)
+                (valueAction ?? throw new ArgumentNullException(nameof(valueAction))).Invoke(@try.Value, argument);
+            else if (throwIfError) @try.Throw();
+        }
+
+        /// <summary> Try to execute action with error </summary>
+        /// <param name="errorAction"> Action if error </param>
+        [PublicAPI]
+        public void DoIfError([InstantHandle] Action<Exception> errorAction)
+        {
+            if ((@try ?? throw new ArgumentNullException(nameof(@try))).Success) return;
+            try
+            {
+                @try.Throw();
+            }
+            catch (Exception exception)
+            {
+                (errorAction ?? throw new ArgumentNullException(nameof(errorAction))).Invoke(exception);
+            }
+        }
+    }
+}
